4c377b01620040daafbff2d37f48aadb552f7075
Abhijit808 <abhijitrayarao@gmail.com>
2022-09-17 19:36:46 +0530

    hey

--- js/home.js
+++ js/home.js
@@ -2,7 +2,8 @@ const round = document.querySelectorAll(".round");
 const slides =document.querySelectorAll(".slide");
 const active = document.querySelector('.s-active');
 const section = document.querySelector('.section-hero');
-let counter = 1
+const selection = document.querySelector('.selection')
+let counter = 0;
 round.forEach(ele=>{
     ele.addEventListener('click',(e)=>{
         if(e.target.classList[1]==='prev'){
@@ -12,23 +13,23 @@ round.forEach(ele=>{
 
             }
             console.log(counter+1);
-            section.style.backgroundImage="url('../public/images/herosection.jpg')"; 
+            section.style.backgroundImage="url('./public/images/herosection.jpg')"; 
             console.log(slides[counter+1]);
             
             slides[0].classList.add('s-active');  
             slides[1].classList.remove('s-active');  
             round[0].classList.add('c-active');  
             round[1].classList.remove('c-active');
+        
         }
         if (e.target.classList[1] === 'next') {
-            counter++;
+            counter++;         
             if(counter>1){
                 counter=1;
             }
-            console.log(e.target);
             slides[1].classList.add('s-active');  
             slides[0].classList.remove('s-active');
-            section.style.backgroundImage="url('../public/images/herosection1.jpg')"; 
+            section.style.backgroundImage="url('./public/images/herosection1.jpg')"; 
             round[1].classList.add('c-active');  
             round[0].classList.remove('c-active');            
         }
@@ -37,8 +38,7 @@ round.forEach(ele=>{
 })
 const timer = setInterval(() => {
      if(round[0].classList[1]==='prev'){
-            console.log(counter);
-            section.style.backgroundImage="url('../public/images/herosection.jpg')"; 
+            section.style.backgroundImage="url('./public/images/herosection.jpg')"; 
             slides[0].classList.add('s-active');  
             slides[1].classList.remove('s-active');  
             round[0].classList.add('c-active');  
@@ -46,17 +46,15 @@ const timer = setInterval(() => {
         }
         
         
-},5000);
+},15000);
 const timer1 = setInterval(() => {
     if(round[1].classList[1] === 'next'){
-        console.log("hey!here");
         slides[1].classList.add('s-active');  
         slides[0].classList.remove('s-active');
-        section.style.backgroundImage="url('../public/images/herosection1.jpg')"; 
+        section.style.backgroundImage="url('./public/images/herosection1.jpg')"; 
         round[1].classList.add('c-active');  
         round[0].classList.remove('c-active');          
     }
         
-},10000);
-
+},30000);
 
--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,8 @@
 const burger = document.querySelector(".burger");
 const uid = document.querySelector(".uid");
 const bid = document.querySelectorAll(".b-li");
-
+const about = document.querySelector(".about");
+const body =document.body
 burger.addEventListener('click',()=>{
     uid.classList.toggle('transform');
     for (let index = 0; index < bid.length; index++) {
@@ -11,4 +12,17 @@ burger.addEventListener('click',()=>{
         bid[2].classList.toggle('d-n');
     }
 })
+// window.addEventListener('scroll',(e)=>{
+//     // console.log(window.pageYOffset);
+//     const scrolloffset = window.pageYOffset;
+//     if(scrolloffset > 201) {
+//         body.style.transition="all 400ms ease-in-out ";
+//         body.style.backgroundColor = "#212121"
+//         about.classList.remove("s-opacity-0")
+//     }
+//     else {
+//         body.style.backgroundColor = "#f5f5f5"
+//     }
+    
+// })
 
